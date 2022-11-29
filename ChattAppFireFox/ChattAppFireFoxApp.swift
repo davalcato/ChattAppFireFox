414973628c32dfd48462d470c46ca3e3f1c3cd15
davalcato <davalcato@yahoo.com>
2022-11-29 12:13:41 -0800

    first commit

---
+++ ChattAppFireFox/ChattAppFireFoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattAppFireFoxApp.swift
+//  ChattAppFireFox
+//
+//  Created by Daval Cato on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ChattAppFireFoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChattAppFireFox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChattAppFireFox
+//
+//  Created by Daval Cato on 11/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
